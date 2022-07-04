dcc033bfa2600e5fe4f94498750bedd6508bad7f
feynman <2901954062@qq.com>
2022-07-04 15:05:01 +0800

    wip: vue2 v-model 添加init

---
+++ src/001v-model/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: fg
+ * @Date: 2022-07-04 11:31:48
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-04 13:20:26
+ * @Description: 数据双向绑定入口
+ */
+
+const dom = document.querySelector<HTMLDivElement>('h1')
+console.log('hello', dom)
+
