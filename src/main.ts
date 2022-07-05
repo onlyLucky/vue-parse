564cb2bde4530849b27af45908975082c609e98c
feynman <2901954062@qq.com>
2022-07-05 11:29:12 +0800

    feat: 首页添加列表目录

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import '@/style.css'
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>vue-parse</h1>
+  <a href="/src/001v-model/index.html" target="_self">001.v-model数据双向绑定</a>
 `
