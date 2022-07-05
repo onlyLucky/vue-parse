6746a9e7f37f8c0f2c1a83b68e44e3ff4fcafcda
feynman <2901954062@qq.com>
2022-07-05 11:30:03 +0800

    style: 代码格式修改

--- src/001v-model/index.ts
+++ src/001v-model/index.ts
@@ -2,7 +2,7 @@
  * @Author: fg
  * @Date: 2022-07-04 11:31:48
  * @LastEditors: fg
- * @LastEditTime: 2022-07-04 15:28:01
+ * @LastEditTime: 2022-07-05 11:17:36
  * @Description: 数据双向绑定入口
  */
 
@@ -12,3 +12,4 @@ App.innerHTML = `
   <input type="text" id="ipt">
   <h3 id="txt"></h3>
 `
+
