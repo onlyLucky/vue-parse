9bb2079cff96245e7f7122b86fcfe88e1715573c
feynman <2901954062@qq.com>
2022-07-07 15:43:06 +0800

    feat: 导出ts interface问题提交

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: fg
+ * @Date: 2022-07-07 14:24:30
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-07 15:38:09
+ * @Description: content
+ */
+export { StepPrototype } from './v-model'
