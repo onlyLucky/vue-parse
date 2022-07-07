9edfe31aa04ca705e0165c5f14ecd4697febb0c5
feynman <2901954062@qq.com>
2022-07-07 15:02:28 +0800

    workflow: 新增全局变量d.ts

---
+++ src/global.d.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: fg
+ * @Date: 2022-07-07 14:32:39
+ * @LastEditors: fg
+ * @LastEditTime: 2022-07-07 14:46:27
+ * @Description: 全局范围访问的库
+ */
+1
+declare const __DEV__: boolean
+
+interface TEST {
+  txt: Array<string>
+}
